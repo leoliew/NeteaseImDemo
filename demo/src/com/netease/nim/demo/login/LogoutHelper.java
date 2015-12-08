fc4b6dff615aa5c6175ad35ef61e2a0b70ab1379
Leo <lzm1093038078@gmail.com>
2015-12-08 22:25:33 +0800

    init project

---
+++ demo/src/com/netease/nim/demo/login/LogoutHelper.java
@@ -0,0 +1,18 @@
+package com.netease.nim.demo.login;
+
+import com.netease.nim.demo.DemoCache;
+import com.netease.nim.uikit.LoginSyncDataStatusObserver;
+import com.netease.nim.uikit.NimUIKit;
+
+/**
+ * 注销帮助类
+ * Created by huangjun on 2015/10/8.
+ */
+public class LogoutHelper {
+    public static void logout() {
+        // 清理缓存&注销监听&清除状态
+        NimUIKit.clearCache();
+        DemoCache.clear();
+        LoginSyncDataStatusObserver.getInstance().reset();
+    }
+}
